bea4f10d8e99efbd3b75d0fbf92c1a1c3b9da353
Joy Brar <joybrar2001@gmail.com>
2023-08-07 22:52:45 +0530

    Finished App

--- components/Home.js
+++ components/Home.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, SafeAreaView, Image, FlatList, ScrollView } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView, Image, FlatList, ScrollView, TouchableOpacity } from 'react-native'
 import React from 'react'
 import Feather from 'react-native-vector-icons/Feather'
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
@@ -9,7 +9,7 @@ import popularData from '../assets/data/popularData'
 Feather.loadFont();
 MaterialCommunityIcons.loadFont();
 
-const Home = () => {
+const Home = ({ navigation }) => {
 
   const renderCategoryItem = ({ item }) => {
     return (
@@ -35,89 +35,96 @@ const Home = () => {
 
   return (
     <View style={styles.container}>
-      <ScrollView 
+      <ScrollView
         contentInsetAdjustmentBehavior='automatic'
         showsVerticalScrollIndicator={false}
       >
-      <SafeAreaView>
-        <View style={styles.headerWrapper}>
-          <Image source={require('../assets/images/profile.png')} style={styles.profileImage} />
-          <Feather name='menu' size={24} color={colors.textDark} />
-        </View>
-      </SafeAreaView>
-
-      <View style={styles.titleWrapper}>
-        <Text style={styles.titleSubtitle}>Food</Text>
-        <Text style={styles.titleTitle}>Delivery</Text>
-      </View>
-
-      <View style={styles.serachWrapper}>
-        <Feather name='search' size={16} color={colors.textDark} />
-        <View style={styles.search}>
-          <Text style={styles.searchText}>Search</Text>
-        </View>
-      </View>
-
-      <View style={styles.categoriesWrapper}>
-        <Text style={styles.categoriesTitle}>Categories</Text>
-        <View style={styles.categoriesListWrapper}>
-          <FlatList
-            data={categoriesData}
-            renderItem={renderCategoryItem}
-            keyExtractor={item => item.id}
-            horizontal={true}
-          />
-        </View>
-      </View>
-
-      <View style={styles.popularWrapper}>
-        <Text style={styles.popularTitle}>Popular</Text>
-        {popularData.map((item, index) => (
-          <View style={[styles.popularCardWrapper, {
-            marginTop: item.id === 1 ? 10 : 20,
-          }]} key={index}>
-            <View>
-              <View>
-                <View style={styles.popularTopWrapper}>
-                  <MaterialCommunityIcons
-                    name='crown'
-                    size={12}
-                    color={colors.primary}
-                  />
-                  <Text style={styles.popularTopText}>Top of the week</Text>
-                </View>
-                <View style={styles.popularTitlesWrapper}>
-                  <Text style={styles.popularTitlesTitle}>{item.title}</Text>
-                  <Text style={styles.popularTitlesWeight}>Weight {item.weight}</Text>
-                </View>
-              </View>
-              <View style={styles.popularCardBottom}>
-                <View style={styles.addPizzaButton}>
-                  <Feather
-                    name='plus'
-                    size={10}
-                    color={colors.textDark}
-                  />
-                </View>
-                <View style={styles.ratingWrapper}>
-                  <MaterialCommunityIcons
-                    name='star'
-                    size={10}
-                    color={colors.textDark}
-                  />
-                  <Text style={styles.rating}>{item.rating}</Text>
-                </View>
-              </View>
-            </View>
-            <View style={styles.popularCardRight}>
-              <Image
-                source={item.image}
-                style={styles.popularCardImage}
-              />
-            </View>
+        <SafeAreaView>
+          <View style={styles.headerWrapper}>
+            <Image source={require('../assets/images/profile.png')} style={styles.profileImage} />
+            <Feather name='menu' size={24} color={colors.textDark} />
           </View>
-        ))}
-      </View>
+        </SafeAreaView>
+
+        <View style={styles.titleWrapper}>
+          <Text style={styles.titleSubtitle}>Food</Text>
+          <Text style={styles.titleTitle}>Delivery</Text>
+        </View>
+
+        <View style={styles.serachWrapper}>
+          <Feather name='search' size={16} color={colors.textDark} />
+          <View style={styles.search}>
+            <Text style={styles.searchText}>Search</Text>
+          </View>
+        </View>
+
+        <View style={styles.categoriesWrapper}>
+          <Text style={styles.categoriesTitle}>Categories</Text>
+          <View style={styles.categoriesListWrapper}>
+            <FlatList
+              data={categoriesData}
+              renderItem={renderCategoryItem}
+              keyExtractor={item => item.id}
+              horizontal={true}
+            />
+          </View>
+        </View>
+
+        <View style={styles.popularWrapper}>
+          <Text style={styles.popularTitle}>Popular</Text>
+          {popularData.map((item, index) => (
+            <TouchableOpacity 
+              key={index}
+              onPress={() => navigation.navigate('Details',{
+                item: item,
+              })}
+            >
+              <View style={[styles.popularCardWrapper, {
+                marginTop: item.id === 1 ? 10 : 20,
+              }]}>
+                <View>
+                  <View>
+                    <View style={styles.popularTopWrapper}>
+                      <MaterialCommunityIcons
+                        name='crown'
+                        size={12}
+                        color={colors.primary}
+                      />
+                      <Text style={styles.popularTopText}>Top of the week</Text>
+                    </View>
+                    <View style={styles.popularTitlesWrapper}>
+                      <Text style={styles.popularTitlesTitle}>{item.title}</Text>
+                      <Text style={styles.popularTitlesWeight}>Weight {item.weight}</Text>
+                    </View>
+                  </View>
+                  <View style={styles.popularCardBottom}>
+                    <View style={styles.addPizzaButton}>
+                      <Feather
+                        name='plus'
+                        size={10}
+                        color={colors.textDark}
+                      />
+                    </View>
+                    <View style={styles.ratingWrapper}>
+                      <MaterialCommunityIcons
+                        name='star'
+                        size={10}
+                        color={colors.textDark}
+                      />
+                      <Text style={styles.rating}>{item.rating}</Text>
+                    </View>
+                  </View>
+                </View>
+                <View style={styles.popularCardRight}>
+                  <Image
+                    source={item.image}
+                    style={styles.popularCardImage}
+                  />
+                </View>
+              </View>
+            </TouchableOpacity>
+          ))}
+        </View>
       </ScrollView>
     </View>
   )
